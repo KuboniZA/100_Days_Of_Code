c8784cd09b4b7666319c75e19f65a1cdd0ebeb50
KuboniZA <sipumzo.lucwaba@gmail.com>
2026-01-15 21:42:01 +0200

    Add prettytable.

--- Day_16/main.py
+++ Day_16/main.py
@@ -1,1 +1,17 @@
-import turtle
+# import turtle
+# timmy = turtle.Turtle()
+
+# Rather than the above, import the class:
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+print(timmy)
+timmy.shape("turtle")
+timmy.fillcolor("chartreuse")
+timmy.fd(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
